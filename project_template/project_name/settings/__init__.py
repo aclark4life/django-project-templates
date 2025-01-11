de02b73f1a32e6860aebef592ba6ffc02d1b8a69
Jeffrey A. Clark <aclark@aclark.net>
2025-01-11 17:29:53 -0500

    Update templates

--- project_template/project_name/settings/__init__.py
+++ project_template/project_name/settings/__init__.py
@@ -86,3 +86,13 @@ TEMPLATES = [
     },
 ]
 
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
+]
