4e6fb081ae1520774817bb38e1dcf6b8b919b353
Jeffrey A. Clark <aclark@aclark.net>
2025-01-12 13:36:17 -0500

    Update templates

--- project_template/project_name/urls.py
+++ project_template/project_name/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
+from django.contrib import admin
 from .views import BaseView
 
 
 urlpatterns = [
-    path("django/", include("django.contrib.admin.urls")),
+    path("django/", admin.site.urls),
     path("wagtail/", include("wagtail.admin.urls")),
     path("", BaseView.as_view(), name="base")
 ]
