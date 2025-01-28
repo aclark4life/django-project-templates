d4a146bf1f67487fbc0a929635775aa94a766d0f
Jeffrey A. Clark <aclark@aclark.net>
2025-01-28 18:45:09 -0500

    Add telepath adapter

--- home_template/wagtail_hooks.py
+++ home_template/wagtail_hooks.py
@@ -3,6 +3,4 @@ from wagtail import hooks
 @hooks.register("insert_editor_js")
 def add_objectid_js():
     return """
-    {% load static %}
-    <script src="{% static 'js/bson_adapter.js' %}"></script>
     """
