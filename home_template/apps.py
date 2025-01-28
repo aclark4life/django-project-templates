ef0c4cb5fdc58d1211b6be4fc1fc95f498f4210e
Jeffrey A. Clark <aclark@aclark.net>
2025-01-28 18:47:18 -0500

    Add telepath adapter

--- home_template/apps.py
+++ home_template/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class HomeAppConfig(AppConfig):
     default_auto_field = "django_mongodb_backend.fields.ObjectIdAutoField"
     name = "home"
+
+
+    def ready(self):
+        from . import telepath_adapters.bson_adapter
