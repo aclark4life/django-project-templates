84eebc09931193a1ef984ecde60f0cb54e952116
Jeffrey A. Clark <aclark@aclark.net>
2025-02-11 14:01:23 -0500

    Remove adapter

--- home_template/apps.py
+++ home_template/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class HomeAppConfig(AppConfig):
     default_auto_field = "django_mongodb_backend.fields.ObjectIdAutoField"
     name = "home"
-
-
-    def ready(self):
-        from . import telepath_adapters.bson_adapter
